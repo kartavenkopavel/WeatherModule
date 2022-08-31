3b9458f9a543a88293a2b2d00207278718e6c98d
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-08-31 22:32:44 +0300

    A new class was created and a weather table was added

---
+++ src/weather/WeatherSimple.java
@@ -0,0 +1,31 @@
+package weather;
+
+public class WeatherSimple {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+
+    public static void main(String[] args) {
+
+        String cityName = "London";
+        String countryCode = "GB";
+        String currentDate = "Wen,Aug 31";
+        String tomorrowDate = "Thu,Sep 01";
+        String currentFallout = "Scattered clouds. Moderate breeze.";
+        String tomorrowFallout = "Overcast clouds. Moderate breeze.";
+        int currentMaxTemp = 23;
+        int currentMinTemp = 15;
+        int tomorrowMaxTemp = 23;
+        int tomorrowMinTemp = 17;
+
+        System.out.println(cityName+", "+countryCode +"\n-------------------------------------------------------\n"+
+                currentDate+"\n-------------------------------------------------------\n"+
+                "~~~\t\t" + currentFallout + "\n~~~\t\tThe high will be "+currentMaxTemp+"°C, the low will be "+currentMinTemp+"°C"+
+                "\n\n\t\t\t  Morning\tAfternoon\tEvening\t Night\nTEMPERATURE\t\t15°C\t  22°C\t\t21°C\t  17°C\n" +
+                "FEELS LIKE\t\t15°C\t  22°C\t\t20°C\t  16°C\n-------------------------------------------------------");
+        System.out.println(tomorrowDate+"\n-------------------------------------------------------\n"+
+                "~~~\t\t" + tomorrowFallout + "\n"+ANSI_BLACK+"~~~"+ANSI_RESET
+                +"\t\tThe high will be "+tomorrowMaxTemp+"°C, the low will be "+tomorrowMinTemp+"°C"+
+                "\n\n\t\t\t  Morning\tAfternoon\tEvening\t Night\nTEMPERATURE\t\t17°C\t  23°C\t\t21°C\t  18°C\n" +
+                "FEELS LIKE\t\t17°C\t  23°C\t\t21°C\t  18°C\n-------------------------------------------------------");
+    }
+}
