d3d1e538d5c75d6f6a6b7c26c0328890d9567a96
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-09-01 01:31:24 +0300

    Changed method currentWeather from String to String[]

--- src/weather/Current.java
+++ src/weather/Current.java
@@ -9,14 +9,14 @@ import java.net.URL;
 import static weather.Weather.cityName;
 
 public class Current {
-    public static String currentWeather() throws Exception {
+    public static String[] currentWeather() throws Exception {
         /* переменная units вынесена для того, что в дальнейшем можно сделать метод для
         отображения единиц измерения.
         */
         String units = "metric";
         String apiKey = "45d8535b361a7326ac202430c9bb89b3";
         String url = "https://api.openweathermap.org/data/2.5/weather?q="
-                +cityName()+"&units="+units+"&lang=ru&appid="+apiKey;
+                +cityName()+"&units="+units+"&lang=en&appid="+apiKey;
 
         URL urls = new URL(url);
         HttpURLConnection con = (HttpURLConnection) urls.openConnection();
@@ -31,10 +31,11 @@ public class Current {
         }
         in.close();
 
+        String fallout = response.toString().substring(response.indexOf("\"main\""), response.indexOf("\"icon\"")).replace(",", "\n");
+        String temperature = response.toString().substring(response.indexOf("\"temp\""), response.indexOf("\"temp_min\"")).replace(",", "°C\n");
 
-        String s = response.toString().substring(response.indexOf("\"temp\""), response.indexOf("\"p")).replace(",", "`C\n");
+        return new String[]{fallout, temperature};
 
-        return s;
 
     }
 
