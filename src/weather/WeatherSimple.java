ef8902127dd28f2dfc961d69b7e9f599c11d8ec6
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-09-01 22:02:24 +0300

    added weather smiles

--- src/weather/WeatherSimple.java
+++ src/weather/WeatherSimple.java
@@ -19,12 +19,11 @@ public class WeatherSimple {
 
         System.out.println(cityName+", "+countryCode +"\n-------------------------------------------------------\n"+
                 currentDate+"\n-------------------------------------------------------\n"+
-                "~~~\t\t" + currentFallout + "\n~~~\t\tThe high will be "+currentMaxTemp+"°C, the low will be "+currentMinTemp+"°C"+
+                "\u26C5\u26C5\t" + currentFallout + "\n   \t\tThe high will be "+currentMaxTemp+"°C, the low will be "+currentMinTemp+"°C"+
                 "\n\n\t\t\t  Morning\tAfternoon\tEvening\t Night\nTEMPERATURE\t\t15°C\t  22°C\t\t21°C\t  17°C\n" +
                 "FEELS LIKE\t\t15°C\t  22°C\t\t20°C\t  16°C\n-------------------------------------------------------");
         System.out.println(tomorrowDate+"\n-------------------------------------------------------\n"+
-                "~~~\t\t" + tomorrowFallout + "\n"+ANSI_BLACK+"~~~"+ANSI_RESET
-                +"\t\tThe high will be "+tomorrowMaxTemp+"°C, the low will be "+tomorrowMinTemp+"°C"+
+                "\u2601\u2601\t" + tomorrowFallout + "\n   \t\tThe high will be "+tomorrowMaxTemp+"°C, the low will be "+tomorrowMinTemp+"°C"+
                 "\n\n\t\t\t  Morning\tAfternoon\tEvening\t Night\nTEMPERATURE\t\t17°C\t  23°C\t\t21°C\t  18°C\n" +
                 "FEELS LIKE\t\t17°C\t  23°C\t\t21°C\t  18°C\n-------------------------------------------------------");
     }
