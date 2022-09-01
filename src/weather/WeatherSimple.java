49256eb916a8d9d87c5f7b3a6348bdaa8bdcf725
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-09-01 22:03:09 +0300

    -

--- src/weather/WeatherSimple.java
+++ src/weather/WeatherSimple.java
@@ -1,8 +1,6 @@
 package weather;
 
 public class WeatherSimple {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
 
     public static void main(String[] args) {
 
